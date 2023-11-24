8ec870021eb7b594ee951ba1e48e3546f71906ad
JJHugoJJ <150568011+JJHugoJJ@users.noreply.github.com>
2023-11-23 23:07:15 -0300

    Atualizando os arquivos de novo
    To quase terminando

--- script.js
+++ script.js
@@ -13,7 +13,16 @@ const textosDinamicos = [
     " O avanço em tecnologias de armazenamento, como baterias de íon de lítio, está tornando a energia limpa mais acessível e eficiente.",
     " O uso de energia limpa não apenas reduz as emissões de carbono, mas também impulsiona o desenvolvimento sustentável, criando empregos e melhorando a qualidade de vida.",
     " Em algumas áreas, pode haver uma falta de acesso a tecnologias de energia limpa devido à disponibilidade limitada ou à falta de fornecedores locais.",
-    "Uma das principais barreiras do uso residencial da Energia Limpa é o custo inicial."
+    " Uma das principais barreiras do uso residencial da Energia Limpa é o custo inicial.",
+    " Embora a energia hidrelétrica seja uma fonte limpa, a construção de barragens pode causar impactos nos ecossistemas aquáticos, alterando o fluxo dos rios e afetando a vida selvagem.",
+    " Algumas comunidades podem resistir à instalação de infraestrutura de energia limpa em suas proximidades devido ao fenômeno 'Not In My Backyard' (NIMBY), que reflete resistência local a projetos mesmo quando são benéficos globalmente.",
+    " A biomassa pode ser produzida a partir de resíduos agrícolas e florestais, contribuindo para a gestão sustentável de resíduos e fornecendo uma fonte renovável de energia.",
+    " Nas regiões onde a energia geotérmica é abundante, as plantas geotérmicas podem operar continuamente, fornecendo uma fonte constante e confiável de eletricidade.",
+    " Alguns aterros sanitários capturam gases gerados pelo lixo em decomposição para produzir eletricidade, transformando resíduos em uma fonte útil de energia.",
+    " Pequenas ações diárias, como desligar aparelhos eletrônicos quando não estiverem em uso, podem ter um impacto significativo na redução do consumo de energia ao longo do tempo.",
+    " Pequenas ações diárias, como desligar aparelhos eletrônicos quando não estiverem em uso, podem ter um impacto significativo na redução do consumo de energia ao longo do tempo.",
+    " Orcas são um dos animais mais inteligentes do mundo, cada grupo de orca tem uma lingua e cultura propria",
+    " Os criadores da excelente série The OA lançaram uma série nova chamada Assasinato no Fim do Mundo e está passando no Star+, assistam pfv!!"
 ];
 
 // Função para escolher um texto aleatório
